5a0d00e2669e24f874116c00ab8dcaa74f7114a3
sujaybn <89024189+sujaybn@users.noreply.github.com>
2023-09-21 14:19:12 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,60 +1,66 @@
-import {useEffect, useState} from "react";
 //390d460f
+import { useEffect, useState } from "react";
 import './App.css';
 import searchIcon from './search.svg';
 import MovieCard from './MovieCard';
+
 const API_URL = 'http://www.omdbapi.com?apikey=390d460f';
 
-const movie1 = {
-    "Title": "The Amazing Spiderman 2 Webb Cut",
-    "Year": "2021",
-    "imdbID": "tt18351128",
-    "Type": "movie",
-    "Poster": "https://m.media-amazon.com/images/M/MV5BYzYzZDViNWYtNWViMS00NDMxLThlN2YtZjFkOWMwODkzNzhiXkEyXkFqcGdeQXVyMTUwMzM4NzU0._V1_SX300.jpg"
-}
-
 const App = () => {
-    const [movies, setMovies] =useState ([]);
-    const [searchTerm, setSearchTerm] = useState('');
-    const searchMovies = async (title) => {
-    const response = await fetch (`${API_URL}&s=${title}`);
+  const [movies, setMovies] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const searchMovies = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`);
     const data = await response.json();
     setMovies(data.Search);
+  }
+
+  const handleSearch = () => {
+    searchMovies(searchTerm);
+  }
+
+  const handleKeyPress = (e) => {
+    if (e.key === "Enter") {
+      handleSearch();
     }
-    useEffect (()=> {
-        searchMovies('SpiderMan')
-    }, [])
-    
-    return(
-        <div className="app">
-            <h1>MovieLand</h1>
-            <div className="search">
-                <input 
-                placeholder="Search for movies"
-                value = {searchTerm}
-                onChange={(e) => setSearchTerm(e.target.value)}
-                />
-                <img
-                src = {searchIcon} 
-                alt="search"
-                onClick={()=>searchMovies(searchTerm)}
-                />
-            </div>
-            {movies?.length > 0 
-                ? (
-                    <div className="container">
-                        {movies.map((movie) => (
-                            <MovieCard movie={movie}/>
-                        ))}
-                     </div>
-                ) :
-                (
-                    <div className="empty">
-                    <h2> No movies found</h2>
-                    </div>
-                )}
-        </div>
-        );
+  }
+
+  useEffect(() => {
+    searchMovies('SpiderMan');
+  }, [])
+
+  return (
+    <div className="app">
+      <h1>MovieLand</h1>
+      <div className="search">
+        <input
+          placeholder="Search for movies"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+          onKeyPress={handleKeyPress} // Add event listener for Enter key press
+        />
+        <img
+          src={searchIcon}
+          alt="search"
+          onClick={handleSearch}
+        />
+      </div>
+      {movies?.length > 0
+        ? (
+          <div className="container">
+            {movies.map((movie) => (
+              <MovieCard movie={movie} key={movie.imdbID} />
+            ))}
+          </div>
+        ) :
+        (
+          <div className="empty">
+            <h2>No movies found</h2>
+          </div>
+        )}
+    </div>
+  );
 }
 
 export default App;
